f59350828bc35dd3709d1f8d5a9dbc49fa4b94aa
Hansanghyeon <999@hyeon.pro>
2022-10-17 14:05:36 +0000

    feat: tailwind 설정

--- apps/proposal-a/src/pages/index.tsx
+++ apps/proposal-a/src/pages/index.tsx
@@ -11,7 +11,9 @@ const Home: NextPage = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main></main>
+      <main>
+        <div className='text-[56px]'>HELLO WORLD</div>
+      </main>
       <footer></footer>
     </>
   )
